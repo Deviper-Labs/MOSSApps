cfb6d766860d709cffd6c1cb5f4fc0622c000ef3
Mohammad Omar S <94236245+Deviper-Labs@users.noreply.github.com>
2025-06-05 20:09:04 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,2 +1,6 @@
 
-print("Hello I am a file compiled with Nuitka")
+print("Hello World!")
+print("I am a file compiled with Nuitka.")
+print("I also have Github Actions with Nuitka-Action")
+print("used to compile for multiple platforms.")
+
